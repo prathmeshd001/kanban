3b2f5cefde2a04cd3b360d2899a03e4e8db59aa0
Prathmesh Doiphode <psdoiphode2000@gmail.com>
2023-06-19 09:57:14 +0530

    Changes to be committed: 	modified:   src/App.css 	modified:   src/App.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Dashboard from "./pages/dashboard/Dashboard";
 import "./App.css";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "Inter, sans-serif",
+    },
+});
 
 function App() {
     return (
-        <div className="app">
+        <div>
             <BrowserRouter>
                 <CssBaseline />
-                <Routes>
-                    <Route element={<Layout />}>
-                        <Route path="/" element={<Dashboard />} />
-                    </Route>
-                </Routes>
+                <ThemeProvider theme={theme}>
+                    <Routes>
+                        <Route element={<Layout />}>
+                            <Route path="/" element={<Dashboard />} />
+                        </Route>
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </div>
     );
